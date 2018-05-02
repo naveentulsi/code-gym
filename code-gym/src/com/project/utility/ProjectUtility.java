cd2ef9436b2c5cd4d26ceb6787ec15d2b6cb8641
starrsign <32303381+starrsign@users.noreply.github.com>
2018-05-02 12:05:34 +0530

    Some changes

---
+++ code-gym/src/com/project/utility/ProjectUtility.java
@@ -0,0 +1,12 @@
+package com.project.utility;
+
+import java.io.FileReader;
+
+
+public class ProjectUtility {
+	
+	private static final String FILE_NAME = "properties.properties";	
+	private FileReader readProp;
+
+	
+}
